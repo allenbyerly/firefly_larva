fe65c91185095437caa8aaaf6098db623bf2a859
Luis G Romero <luisgromero@live.com>
2014-02-15 13:25:32 -0800

    Initial firefly respository config. Added heroku gem config for deployment.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FireflyApp::Application.config.secret_key_base = 'ba2ee558f73115463fbb2606f7d1fd3e3127795dfa0715c0f1a781e5f5896dbb82c28a271f9b4bd3dd22d0688a4b8eec0290da91a56b93b3f8894197a302d0db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FireflyApp::Application.config.session_store :cookie_store, key: '_Firefly_app_session'
