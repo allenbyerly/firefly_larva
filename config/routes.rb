a3fb8afdbec3ab9b87f129a197c2507b0b41c2b9
Luis G Romero <luisgromero@live.com>
2014-02-15 15:54:09 -0800

    Added precompile option for Heroku and authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 FireflyApp::Application.routes.draw do
   devise_for :users
   get "site/index"
+  get "site/kevin"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
