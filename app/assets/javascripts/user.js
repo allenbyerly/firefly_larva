fcb2389eb4d5973288ceb3ea43bebe74a580bd76
Luis G Romero <luisgromero@live.com>
2014-02-16 15:23:51 -0800

    Added some jquery functionality
    No work.

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -2,3 +2,8 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://coffeescript.org/
 
+$(document).on('click','button#user_save',function(){
+    e.preventDefault();
+   //$(this).parent('div.border-form-div').hide();
+
+});
